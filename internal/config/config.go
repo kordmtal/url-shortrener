bd8e3971be00ca79250c3d64fa6e0ceb92da2f12
kordmtal <kordmtal@gmail.com>
2026-01-23 23:23:43 +0700

    refactor(config): Standardize base URL configuration and normalization

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	"os"
+	"strings"
 )
 
 const (
@@ -25,9 +26,24 @@ func Parse() *Config {
 
 	flag.Parse()
 
-	if envAddr := os.Getenv("ADDRESS"); envAddr != "" {
+	if envAddr := os.Getenv("SERVER_ADDRESS"); envAddr != "" {
 		cfg.ServerAddress = envAddr
 	}
+	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
+		cfg.BasicURLServerAdress = envBaseURL
+	}
+
+	cfg.BasicURLServerAdress = normalizeBaseURL(cfg.BasicURLServerAdress)
 
 	return cfg
+}
+
+func normalizeBaseURL(u string) string {
+	if !strings.HasPrefix(u, "http://") && !strings.HasPrefix(u, "https://") {
+		u = "http://" + u
+	}
+	if !strings.HasSuffix(u, "/") {
+		u += "/"
+	}
+	return u
 }
